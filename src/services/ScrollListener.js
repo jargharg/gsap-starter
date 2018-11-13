d133827111ed26f54d743c86a5afd904fcd41f7d
Jarod Hargreaves <jarod.hargreaves@oneiota.co.uk>
2018-11-13 13:02:04 +0000

    Call action with progress through timeline instead of scrollY

--- src/services/ScrollListener.js
+++ src/services/ScrollListener.js
@@ -9,10 +9,11 @@ class ScrollListener {
 			for (let i = 0; i < this.actions.length; i++) {
 				const action = this.actions[i];
 				if (
-					this.lastScrollY > action.startHeight &&
-					this.lastScrollY < action.endHeight
+					this.lastScrollY >= action.startHeight &&
+					this.lastScrollY <= action.endHeight
 				) {
-					action.action(this.lastScrollY);
+					const progress = (this.lastScrollY - action.startHeight) / action.endHeight;
+					action.action(progress);
 				}
 			}
 
@@ -37,12 +38,13 @@ class ScrollListener {
 	/**
 	 * Add an action to the scroll listener
 	 * @param {Object} action
-	 * @param {Function} action.action - The action to call, with a parameter of scrollY
-	 * @param {String} action.startHeight - The scroll height at which to start calling the action
-	 * @param {String} action.endHeight - The scroll height at which to stop calling the action
+	 * @param {Function} action.action - The action to call with a parameter of progress through (between 0 and 1)
+	 * @param {Number} action.startHeight - The scroll height at which to start calling the action
+	 * @param {Number} action.endHeight - The scroll height at which to stop calling the action
 	 */
 	addAction(action) {
 		this.actions.push(action);
 	}
 }
+
 export default new ScrollListener();
