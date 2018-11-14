ecea92414e1e2c274902c09b753aab4bf4ad4ab5
Jarod <jarod.hargreaves@gmail.com>
2018-11-14 19:51:57 +0000

    Add sine wave animation

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,16 @@
 <script>
 import { TimelineLite, Power2 } from 'gsap';
 import Animation1 from '@/components/Animation1';
+import Animation2 from '@/components/Animation2';
 
 export default {
 	name: 'App',
-	components: { Animation1 },
+	components: { Animation1, Animation2 },
+	data() {
+		return {
+			screenWidth: document.body.offsetWidth,
+		};
+	},
 };
 </script>
 
